4dfe9fb3695b3c1a113ddbc2e29b46a422171023
PikalaxALT <PikalaxALT@gmail.com>
2015-07-09 03:09:31 -0400

    Initial commit

---
+++ main.asm
@@ -0,0 +1,384 @@
+
+SECTION "bank00", ROM0[$00]
+INCBIN "binsplit/bank00.bin"
+
+SECTION "bank01", ROMX, BANK[$01]
+INCBIN "binsplit/bank01.bin"
+
+SECTION "bank02", ROMX, BANK[$02]
+INCBIN "binsplit/bank02.bin"
+
+SECTION "bank03", ROMX, BANK[$03]
+INCBIN "binsplit/bank03.bin"
+
+SECTION "bank04", ROMX, BANK[$04]
+INCBIN "binsplit/bank04.bin"
+
+SECTION "bank05", ROMX, BANK[$05]
+INCBIN "binsplit/bank05.bin"
+
+SECTION "bank06", ROMX, BANK[$06]
+INCBIN "binsplit/bank06.bin"
+
+SECTION "bank07", ROMX, BANK[$07]
+INCBIN "binsplit/bank07.bin"
+
+SECTION "bank08", ROMX, BANK[$08]
+INCBIN "binsplit/bank08.bin"
+
+SECTION "bank09", ROMX, BANK[$09]
+INCBIN "binsplit/bank09.bin"
+
+SECTION "bank0a", ROMX, BANK[$0a]
+INCBIN "binsplit/bank0a.bin"
+
+SECTION "bank0b", ROMX, BANK[$0b]
+INCBIN "binsplit/bank0b.bin"
+
+SECTION "bank0c", ROMX, BANK[$0c]
+INCBIN "binsplit/bank0c.bin"
+
+SECTION "bank0d", ROMX, BANK[$0d]
+INCBIN "binsplit/bank0d.bin"
+
+SECTION "bank0e", ROMX, BANK[$0e]
+INCBIN "binsplit/bank0e.bin"
+
+SECTION "bank0f", ROMX, BANK[$0f]
+INCBIN "binsplit/bank0f.bin"
+
+SECTION "bank10", ROMX, BANK[$10]
+INCBIN "binsplit/bank10.bin"
+
+SECTION "bank11", ROMX, BANK[$11]
+INCBIN "binsplit/bank11.bin"
+
+SECTION "bank12", ROMX, BANK[$12]
+INCBIN "binsplit/bank12.bin"
+
+SECTION "bank13", ROMX, BANK[$13]
+INCBIN "binsplit/bank13.bin"
+
+SECTION "bank14", ROMX, BANK[$14]
+INCBIN "binsplit/bank14.bin"
+
+SECTION "bank15", ROMX, BANK[$15]
+INCBIN "binsplit/bank15.bin"
+
+SECTION "bank16", ROMX, BANK[$16]
+INCBIN "binsplit/bank16.bin"
+
+SECTION "bank17", ROMX, BANK[$17]
+INCBIN "binsplit/bank17.bin"
+
+SECTION "bank18", ROMX, BANK[$18]
+INCBIN "binsplit/bank18.bin"
+
+SECTION "bank19", ROMX, BANK[$19]
+INCBIN "binsplit/bank19.bin"
+
+SECTION "bank1a", ROMX, BANK[$1a]
+INCBIN "binsplit/bank1a.bin"
+
+SECTION "bank1b", ROMX, BANK[$1b]
+INCBIN "binsplit/bank1b.bin"
+
+SECTION "bank1c", ROMX, BANK[$1c]
+INCBIN "binsplit/bank1c.bin"
+
+SECTION "bank1d", ROMX, BANK[$1d]
+INCBIN "binsplit/bank1d.bin"
+
+SECTION "bank1e", ROMX, BANK[$1e]
+INCBIN "binsplit/bank1e.bin"
+
+SECTION "bank1f", ROMX, BANK[$1f]
+INCBIN "binsplit/bank1f.bin"
+
+SECTION "bank20", ROMX, BANK[$20]
+INCBIN "binsplit/bank20.bin"
+
+SECTION "bank21", ROMX, BANK[$21]
+INCBIN "binsplit/bank21.bin"
+
+SECTION "bank22", ROMX, BANK[$22]
+INCBIN "binsplit/bank22.bin"
+
+SECTION "bank23", ROMX, BANK[$23]
+INCBIN "binsplit/bank23.bin"
+
+SECTION "bank24", ROMX, BANK[$24]
+INCBIN "binsplit/bank24.bin"
+
+SECTION "bank25", ROMX, BANK[$25]
+INCBIN "binsplit/bank25.bin"
+
+SECTION "bank26", ROMX, BANK[$26]
+INCBIN "binsplit/bank26.bin"
+
+SECTION "bank27", ROMX, BANK[$27]
+INCBIN "binsplit/bank27.bin"
+
+SECTION "bank28", ROMX, BANK[$28]
+INCBIN "binsplit/bank28.bin"
+
+SECTION "bank29", ROMX, BANK[$29]
+INCBIN "binsplit/bank29.bin"
+
+SECTION "bank2a", ROMX, BANK[$2a]
+INCBIN "binsplit/bank2a.bin"
+
+SECTION "bank2b", ROMX, BANK[$2b]
+INCBIN "binsplit/bank2b.bin"
+
+SECTION "bank2c", ROMX, BANK[$2c]
+INCBIN "binsplit/bank2c.bin"
+
+SECTION "bank2d", ROMX, BANK[$2d]
+INCBIN "binsplit/bank2d.bin"
+
+SECTION "bank2e", ROMX, BANK[$2e]
+INCBIN "binsplit/bank2e.bin"
+
+SECTION "bank2f", ROMX, BANK[$2f]
+INCBIN "binsplit/bank2f.bin"
+
+SECTION "bank30", ROMX, BANK[$30]
+INCBIN "binsplit/bank30.bin"
+
+SECTION "bank31", ROMX, BANK[$31]
+INCBIN "binsplit/bank31.bin"
+
+SECTION "bank32", ROMX, BANK[$32]
+INCBIN "binsplit/bank32.bin"
+
+SECTION "bank33", ROMX, BANK[$33]
+INCBIN "binsplit/bank33.bin"
+
+SECTION "bank34", ROMX, BANK[$34]
+INCBIN "binsplit/bank34.bin"
+
+SECTION "bank35", ROMX, BANK[$35]
+INCBIN "binsplit/bank35.bin"
+
+SECTION "bank36", ROMX, BANK[$36]
+INCBIN "binsplit/bank36.bin"
+
+SECTION "bank37", ROMX, BANK[$37]
+INCBIN "binsplit/bank37.bin"
+
+SECTION "bank38", ROMX, BANK[$38]
+INCBIN "binsplit/bank38.bin"
+
+SECTION "bank39", ROMX, BANK[$39]
+INCBIN "binsplit/bank39.bin"
+
+SECTION "bank3a", ROMX, BANK[$3a]
+INCBIN "binsplit/bank3a.bin"
+
+SECTION "bank3b", ROMX, BANK[$3b]
+INCBIN "binsplit/bank3b.bin"
+
+SECTION "bank3c", ROMX, BANK[$3c]
+INCBIN "binsplit/bank3c.bin"
+
+SECTION "bank3d", ROMX, BANK[$3d]
+INCBIN "binsplit/bank3d.bin"
+
+SECTION "bank3e", ROMX, BANK[$3e]
+INCBIN "binsplit/bank3e.bin"
+
+SECTION "bank3f", ROMX, BANK[$3f]
+INCBIN "binsplit/bank3f.bin"
+
+SECTION "bank40", ROMX, BANK[$40]
+INCBIN "binsplit/bank40.bin"
+
+SECTION "bank41", ROMX, BANK[$41]
+INCBIN "binsplit/bank41.bin"
+
+SECTION "bank42", ROMX, BANK[$42]
+INCBIN "binsplit/bank42.bin"
+
+SECTION "bank43", ROMX, BANK[$43]
+INCBIN "binsplit/bank43.bin"
+
+SECTION "bank44", ROMX, BANK[$44]
+INCBIN "binsplit/bank44.bin"
+
+SECTION "bank45", ROMX, BANK[$45]
+INCBIN "binsplit/bank45.bin"
+
+SECTION "bank46", ROMX, BANK[$46]
+INCBIN "binsplit/bank46.bin"
+
+SECTION "bank47", ROMX, BANK[$47]
+INCBIN "binsplit/bank47.bin"
+
+SECTION "bank48", ROMX, BANK[$48]
+INCBIN "binsplit/bank48.bin"
+
+SECTION "bank49", ROMX, BANK[$49]
+INCBIN "binsplit/bank49.bin"
+
+SECTION "bank4a", ROMX, BANK[$4a]
+INCBIN "binsplit/bank4a.bin"
+
+SECTION "bank4b", ROMX, BANK[$4b]
+INCBIN "binsplit/bank4b.bin"
+
+SECTION "bank4c", ROMX, BANK[$4c]
+INCBIN "binsplit/bank4c.bin"
+
+SECTION "bank4d", ROMX, BANK[$4d]
+INCBIN "binsplit/bank4d.bin"
+
+SECTION "bank4e", ROMX, BANK[$4e]
+INCBIN "binsplit/bank4e.bin"
+
+SECTION "bank4f", ROMX, BANK[$4f]
+INCBIN "binsplit/bank4f.bin"
+
+SECTION "bank50", ROMX, BANK[$50]
+INCBIN "binsplit/bank50.bin"
+
+SECTION "bank51", ROMX, BANK[$51]
+INCBIN "binsplit/bank51.bin"
+
+SECTION "bank52", ROMX, BANK[$52]
+INCBIN "binsplit/bank52.bin"
+
+SECTION "bank53", ROMX, BANK[$53]
+INCBIN "binsplit/bank53.bin"
+
+SECTION "bank54", ROMX, BANK[$54]
+INCBIN "binsplit/bank54.bin"
+
+SECTION "bank55", ROMX, BANK[$55]
+INCBIN "binsplit/bank55.bin"
+
+SECTION "bank56", ROMX, BANK[$56]
+INCBIN "binsplit/bank56.bin"
+
+SECTION "bank57", ROMX, BANK[$57]
+INCBIN "binsplit/bank57.bin"
+
+SECTION "bank58", ROMX, BANK[$58]
+INCBIN "binsplit/bank58.bin"
+
+SECTION "bank59", ROMX, BANK[$59]
+INCBIN "binsplit/bank59.bin"
+
+SECTION "bank5a", ROMX, BANK[$5a]
+INCBIN "binsplit/bank5a.bin"
+
+SECTION "bank5b", ROMX, BANK[$5b]
+INCBIN "binsplit/bank5b.bin"
+
+SECTION "bank5c", ROMX, BANK[$5c]
+INCBIN "binsplit/bank5c.bin"
+
+SECTION "bank5d", ROMX, BANK[$5d]
+INCBIN "binsplit/bank5d.bin"
+
+SECTION "bank5e", ROMX, BANK[$5e]
+INCBIN "binsplit/bank5e.bin"
+
+SECTION "bank5f", ROMX, BANK[$5f]
+INCBIN "binsplit/bank5f.bin"
+
+SECTION "bank60", ROMX, BANK[$60]
+INCBIN "binsplit/bank60.bin"
+
+SECTION "bank61", ROMX, BANK[$61]
+INCBIN "binsplit/bank61.bin"
+
+SECTION "bank62", ROMX, BANK[$62]
+INCBIN "binsplit/bank62.bin"
+
+SECTION "bank63", ROMX, BANK[$63]
+INCBIN "binsplit/bank63.bin"
+
+SECTION "bank64", ROMX, BANK[$64]
+INCBIN "binsplit/bank64.bin"
+
+SECTION "bank65", ROMX, BANK[$65]
+INCBIN "binsplit/bank65.bin"
+
+SECTION "bank66", ROMX, BANK[$66]
+INCBIN "binsplit/bank66.bin"
+
+SECTION "bank67", ROMX, BANK[$67]
+INCBIN "binsplit/bank67.bin"
+
+SECTION "bank68", ROMX, BANK[$68]
+INCBIN "binsplit/bank68.bin"
+
+SECTION "bank69", ROMX, BANK[$69]
+INCBIN "binsplit/bank69.bin"
+
+SECTION "bank6a", ROMX, BANK[$6a]
+INCBIN "binsplit/bank6a.bin"
+
+SECTION "bank6b", ROMX, BANK[$6b]
+INCBIN "binsplit/bank6b.bin"
+
+SECTION "bank6c", ROMX, BANK[$6c]
+INCBIN "binsplit/bank6c.bin"
+
+SECTION "bank6d", ROMX, BANK[$6d]
+INCBIN "binsplit/bank6d.bin"
+
+SECTION "bank6e", ROMX, BANK[$6e]
+INCBIN "binsplit/bank6e.bin"
+
+SECTION "bank6f", ROMX, BANK[$6f]
+INCBIN "binsplit/bank6f.bin"
+
+SECTION "bank70", ROMX, BANK[$70]
+INCBIN "binsplit/bank70.bin"
+
+SECTION "bank71", ROMX, BANK[$71]
+INCBIN "binsplit/bank71.bin"
+
+SECTION "bank72", ROMX, BANK[$72]
+INCBIN "binsplit/bank72.bin"
+
+SECTION "bank73", ROMX, BANK[$73]
+INCBIN "binsplit/bank73.bin"
+
+SECTION "bank74", ROMX, BANK[$74]
+INCBIN "binsplit/bank74.bin"
+
+SECTION "bank75", ROMX, BANK[$75]
+INCBIN "binsplit/bank75.bin"
+
+SECTION "bank76", ROMX, BANK[$76]
+INCBIN "binsplit/bank76.bin"
+
+SECTION "bank77", ROMX, BANK[$77]
+INCBIN "binsplit/bank77.bin"
+
+SECTION "bank78", ROMX, BANK[$78]
+INCBIN "binsplit/bank78.bin"
+
+SECTION "bank79", ROMX, BANK[$79]
+INCBIN "binsplit/bank79.bin"
+
+SECTION "bank7a", ROMX, BANK[$7a]
+INCBIN "binsplit/bank7a.bin"
+
+SECTION "bank7b", ROMX, BANK[$7b]
+INCBIN "binsplit/bank7b.bin"
+
+SECTION "bank7c", ROMX, BANK[$7c]
+INCBIN "binsplit/bank7c.bin"
+
+SECTION "bank7d", ROMX, BANK[$7d]
+INCBIN "binsplit/bank7d.bin"
+
+SECTION "bank7e", ROMX, BANK[$7e]
+INCBIN "binsplit/bank7e.bin"
+
+SECTION "bank7f", ROMX, BANK[$7f]
+INCBIN "binsplit/bank7f.bin"
