58fa0a54babe98be0890b2bfec083eb504c661a7
Matthew Glazar <strager.nds@gmail.com>
2018-03-25 15:03:55 -0700

    Decode MapGroupPointers

--- maps/map_headers.asm
+++ maps/map_headers.asm
@@ -2,4 +2,133 @@ MapTriggers:: ; 94000
 	dr $94000, $940ed
 
 MapGroupPointers::
-	dr $940ed, $965f9
+	dw MapGroup1
+	dw MapGroup2
+	dw MapGroup3
+	dw MapGroup4
+	dw MapGroup5
+	dw MapGroup6
+	dw MapGroup7
+	dw MapGroup8
+	dw MapGroup9
+	dw MapGroup10
+	dw MapGroup11
+	dw MapGroup12
+	dw MapGroup13
+	dw MapGroup14
+	dw MapGroup15
+	dw MapGroup16
+	dw MapGroup17
+	dw MapGroup18
+	dw MapGroup19
+	dw MapGroup20
+	dw MapGroup21
+	dw MapGroup22
+	dw MapGroup23
+	dw MapGroup24
+	dw MapGroup25
+	dw MapGroup26
+
+
+MapGroup1:
+	dr $94121, $9419f
+
+
+MapGroup2:
+	dr $9419f, $941de
+
+
+MapGroup3:
+	dr $941de, $944c0
+
+
+MapGroup4:
+	dr $944c0, $94511
+
+
+MapGroup5:
+	dr $94511, $9456b
+
+
+MapGroup6:
+	dr $9456b, $945b3
+
+
+MapGroup7:
+	dr $945b3, $9464c
+
+
+MapGroup8:
+	dr $9464c, $9468b
+
+
+MapGroup9:
+	dr $9468b, $946c1
+
+
+MapGroup10:
+	dr $946c1, $9475a
+
+
+MapGroup11:
+	dr $9475a, $94820
+
+
+MapGroup12:
+	dr $94820, $94895
+
+
+MapGroup13:
+	dr $94895, $948cb
+
+
+MapGroup14:
+	dr $948cb, $94913
+
+
+MapGroup15:
+	dr $94913, $9497f
+
+
+MapGroup16:
+	dr $9497f, $949c7
+
+
+MapGroup17:
+	dr $949c7, $94a3c
+
+
+MapGroup18:
+	dr $94a3c, $94aba
+
+
+MapGroup19:
+	dr $94aba, $94ade
+
+
+MapGroup20:
+	dr $94ade, $94b02
+
+
+MapGroup21:
+	dr $94b02, $94bec
+
+
+MapGroup22:
+	dr $94bec, $94c3d
+
+
+MapGroup23:
+	dr $94c3d, $94cb2
+
+
+MapGroup24:
+	dr $94cb2, $94d27
+
+
+MapGroup25:
+	dr $94d27, $94dae
+
+
+MapGroup26:
+	dr $94dae, $965f9
