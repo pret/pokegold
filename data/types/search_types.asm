519fe1eda94386ec39f90331c9b2fe58bbe46e96
libjet <libj3t@gmail.com>
2020-05-19 19:07:50 +0100

    Add pokedex.asm

---
+++ data/types/search_types.asm
@@ -0,0 +1,19 @@
+PokedexTypeSearchConversionTable:
+; entries correspond with PokedexTypeSearchStrings (see data/types/search_strings.asm)
+	db NORMAL
+	db FIRE
+	db WATER
+	db GRASS
+	db ELECTRIC
+	db ICE
+	db FIGHTING
+	db POISON
+	db GROUND
+	db FLYING
+	db PSYCHIC_TYPE
+	db BUG
+	db ROCK
+	db GHOST
+	db DRAGON
+	db DARK
+	db STEEL
