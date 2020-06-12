0c0f9b42bc1426f2dcdff9f9016bb3fa946b4d49
entrpntr <entrpntr@gmail.com>
2020-06-12 00:33:21 -0400

    Revert gold title trail 2bpp change, add comment to title.asm instead.

--- engine/movie/title.asm
+++ engine/movie/title.asm
@@ -40,7 +40,10 @@ TitleScreen:
 	ld a, BANK(TitleScreenGFX4)
 	call FarDecompress
 
-; Decompress Ho-Oh/Lugia sparkle
+; Ho-Oh/Lugia title trail.
+; This should only copy 4 tiles; there are 4 extra whitespace tiles in Gold
+; before Ho-Oh gfx, but Silver reads the first 64 bytes of the compressed
+; Lugia gfx and writes them to VRAM (but never displays them on screen).
 	ld hl, TitleScreenGFX3
 	ld de, vTiles1 tile $78
 	ld bc, 8 tiles
