be3f9cd5dce1a398714bf94a76a660320b5d6a99
Rangi <remy.oukaour+rangi42@gmail.com>
2020-07-06 15:49:31 -0400

    Define SERIAL_PREAMBLE_LENGTH

--- engine/link/link.asm
+++ engine/link/link.asm
@@ -573,7 +573,7 @@ FixDataForLinkTransfer:
 Link_PrepPartyData_Gen1:
 	ld de, wLinkData
 	ld a, SERIAL_PREAMBLE_BYTE
-	ld b, PARTY_LENGTH
+	ld b, SERIAL_PREAMBLE_LENGTH
 .loop1
 	ld [de], a
 	inc de
@@ -739,7 +739,7 @@ Link_PrepPartyData_Gen1:
 Link_PrepPartyData_Gen2:
 	ld de, wLinkData
 	ld a, SERIAL_PREAMBLE_BYTE
-	ld b, PARTY_LENGTH
+	ld b, SERIAL_PREAMBLE_LENGTH
 .loop1
 	ld [de], a
 	inc de
