b4ee57c480cf937ccd68495e7f4a56b3edbeddd8
Rangi <remy.oukaour+rangi42@gmail.com>
2021-03-21 20:09:39 -0400

    Clean up some macro definitions

--- constants/item_constants.asm
+++ constants/item_constants.asm
@@ -200,6 +200,11 @@ NUM_ITEMS EQU const_value - 1
 
 __tmhm_value__ = 1
 
+add_tmnum: MACRO
+\1_TMNUM EQU __tmhm_value__
+__tmhm_value__ = __tmhm_value__ + 1
+ENDM
+
 add_tm: MACRO
 ; Defines three constants:
 ; - TM_\1: the item id, starting at $bf
@@ -217,8 +222,7 @@ endc
 MOVE_FOR_TM = \1
 PURGE MOVE_FOR_TM
 	const TM_\1
-\1_TMNUM EQU __tmhm_value__
-__tmhm_value__ = __tmhm_value__ + 1
+	add_tmnum \1
 ENDM
 
 ; see data/moves/tmhm_moves.asm for moves
@@ -295,8 +299,7 @@ MOVE_FOR_HM = \1
 PURGE MOVE_FOR_HM
 PURGE HM_VALUE
 	const HM_\1
-\1_TMNUM EQU __tmhm_value__
-__tmhm_value__ = __tmhm_value__ + 1
+	add_tmnum \1
 ENDM
 
 	add_hm CUT          ; f3
@@ -308,7 +311,7 @@ ENDM
 	add_hm WATERFALL    ; f9
 NUM_HMS EQU __tmhm_value__ - NUM_TMS - 1
 
-NUM_TM_HM EQU __tmhm_value__ - 1
+NUM_TM_HM EQU NUM_TMS + NUM_HMS
 
 	const ITEM_FA       ; fa
 
