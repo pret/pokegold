57e0864cce21b496d547b5bac78fa1c53a6f2885
Alexis Bastiani <65498421+Zappa-92@users.noreply.github.com>
2022-04-23 16:15:18 -0300

    Update bug_contest_mons.asm

--- data/wild/bug_contest_mons.asm
+++ data/wild/bug_contest_mons.asm
@@ -1,13 +1,13 @@
 ContestMons:
 	;   %, species,   min, max
-	db 20, CATERPIE,    9, 21
-	db 20, WEEDLE,      9, 21
-	db 10, METAPOD,    11, 21
-	db 10, KAKUNA,     11, 21
-	db  5, BUTTERFREE, 14, 18
-	db  5, BEEDRILL,   14, 18
-	db 10, VENONAT,    12, 20
-	db 10, PARAS,      12, 20
-	db  5, SCYTHER,    16, 18
-	db  5, PINSIR,     16, 18
+	db 20, CATERPIE,    9, 22
+	db 20, WEEDLE,      9, 22
+	db 10, METAPOD,    11, 22
+	db 10, KAKUNA,     11, 22
+	db  5, BUTTERFREE, 14, 19
+	db  5, BEEDRILL,   14, 19
+	db  5, HERACROSS,  12, 21
+	db 15, PARAS,      12, 21
+	db  5, SCYTHER,    16, 21
+	db  5, PINSIR,     16, 21
 	db -1, VENOMOTH,   30, 40
