pret/pokecrystal

Define scene constants alongside scene_scripts
Generic SCENE_DEFAULT and SCENE_FINISHED are legacy values

maps/EcruteakPokecenter1F.asm
@@ -7,8 +7,8 @@
 
 EcruteakPokecenter1F_MapScripts:
 	def_scene_scripts
-	scene_script .MeetBill ; SCENE_DEFAULT
-	scene_script .DummyScene ; SCENE_FINISHED
+	scene_script .MeetBill,   SCENE_ECRUTEAKPOKECENTER1F_MEET_BILL
+	scene_script .DummyScene, SCENE_ECRUTEAKPOKECENTER1F_NOTHING
 
 	def_callbacks
 
@@ -51,7 +51,7 @@ EcruteakPokecenter1F_MapScripts:
 	disappear ECRUTEAKPOKECENTER1F_BILL
 	clearevent EVENT_MET_BILL
 	setflag ENGINE_TIME_CAPSULE
-	setscene SCENE_FINISHED
+	setscene SCENE_ECRUTEAKPOKECENTER1F_NOTHING
 	waitsfx
 	end
 

maps/MahoganyTown.asm
@@ -8,8 +8,8 @@ DEF MAHOGANYTOWN_RAGECANDYBAR_PRICE EQU 300
 
 MahoganyTown_MapScripts:
 	def_scene_scripts
-	scene_script .DummyScene0 ; SCENE_DEFAULT
-	scene_script .DummyScene1 ; SCENE_FINISHED
+	scene_script .DummyScene0, SCENE_MAHOGANYTOWN_TRY_RAGECANDYBAR
+	scene_script .DummyScene1, SCENE_MAHOGANYTOWN_NOTHING
 
 	def_callbacks
 	callback MAPCALLBACK_NEWMAP, .FlyPoint
@@ -255,8 +255,8 @@ MahoganyTown_MapEvents:
 	warp_event  9,  1, ROUTE_43_MAHOGANY_GATE, 3
 
 	def_coord_events
-	coord_event 19,  8, SCENE_DEFAULT, MahoganyTownTryARageCandyBarScript
-	coord_event 19,  9, SCENE_DEFAULT, MahoganyTownTryARageCandyBarScript
+	coord_event 19,  8, SCENE_MAHOGANYTOWN_TRY_RAGECANDYBAR, MahoganyTownTryARageCandyBarScript
+	coord_event 19,  9, SCENE_MAHOGANYTOWN_TRY_RAGECANDYBAR, MahoganyTownTryARageCandyBarScript
 
 	def_bg_events
 	bg_event  1,  5, BGEVENT_READ, MahoganyTownSign

maps/MrPokemonsHouse.asm
@@ -4,8 +4,8 @@
 
 MrPokemonsHouse_MapScripts:
 	def_scene_scripts
-	scene_script .MeetMrPokemon ; SCENE_DEFAULT
-	scene_script .DummyScene ; SCENE_FINISHED
+	scene_script .MeetMrPokemon, SCENE_MRPOKEMONSHOUSE_MEET_MR_POKEMON
+	scene_script .DummyScene,    SCENE_MRPOKEMONSHOUSE_NOTHING
 
 	def_callbacks
 
@@ -123,7 +123,7 @@ MrPokemonsHouse_OakScript:
 	setevent EVENT_RIVAL_NEW_BARK_TOWN
 	setevent EVENT_PLAYERS_HOUSE_1F_NEIGHBOR
 	clearevent EVENT_PLAYERS_NEIGHBORS_HOUSE_NEIGHBOR
-	setscene SCENE_FINISHED
+	setscene SCENE_MRPOKEMONSHOUSE_NOTHING
 	setmapscene CHERRYGROVE_CITY, SCENE_CHERRYGROVECITY_MEET_RIVAL
 	setmapscene ELMS_LAB, SCENE_ELMSLAB_MEET_OFFICER
 	specialphonecall SPECIALCALL_ROBBED

maps/Route27.asm
@@ -11,8 +11,8 @@
 
 Route27_MapScripts:
 	def_scene_scripts
-	scene_script .DummyScene0 ; SCENE_DEFAULT
-	scene_script .DummyScene1 ; SCENE_FINISHED
+	scene_script .DummyScene0, SCENE_ROUTE27_FIRST_STEP_INTO_KANTO
+	scene_script .DummyScene1, SCENE_ROUTE27_NOTHING
 
 	def_callbacks
 
@@ -40,7 +40,7 @@ FirstStepIntoKantoScene_Continue:
 	writetext Route27FisherText
 	waitbutton
 	closetext
-	setscene SCENE_FINISHED
+	setscene SCENE_ROUTE27_NOTHING
 	end
 
 Route27FisherScript:
@@ -474,8 +474,8 @@ Route27_MapEvents:
 	warp_event 36,  5, TOHJO_FALLS, 2
 
 	def_coord_events
-	coord_event 18, 10, SCENE_DEFAULT, FirstStepIntoKantoLeftScene
-	coord_event 19, 10, SCENE_DEFAULT, FirstStepIntoKantoRightScene
+	coord_event 18, 10, SCENE_ROUTE27_FIRST_STEP_INTO_KANTO, FirstStepIntoKantoLeftScene
+	coord_event 19, 10, SCENE_ROUTE27_FIRST_STEP_INTO_KANTO, FirstStepIntoKantoRightScene
 
 	def_bg_events
 	bg_event 25,  7, BGEVENT_READ, TohjoFallsSign

pret/pokegold

maps/EcruteakPokecenter1F.asm
@@ -7,8 +7,8 @@
 
 EcruteakPokecenter1F_MapScripts:
 	def_scene_scripts
-	scene_script .MeetBill ; SCENE_DEFAULT
-	scene_script .DummyScene ; SCENE_FINISHED
+	scene_script .MeetBill,   SCENE_ECRUTEAKPOKECENTER1F_MEET_BILL
+	scene_script .DummyScene, SCENE_ECRUTEAKPOKECENTER1F_NOOP
 
 	def_callbacks
 
@@ -51,7 +51,7 @@ EcruteakPokecenter1F_MapScripts:
 	disappear ECRUTEAKPOKECENTER1F_BILL
 	clearevent EVENT_MET_BILL
 	setflag ENGINE_TIME_CAPSULE
-	setscene SCENE_FINISHED
+	setscene SCENE_ECRUTEAKPOKECENTER1F_NOOP
 	waitsfx
 	end
 

maps/MahoganyTown.asm
@@ -8,8 +8,8 @@ DEF MAHOGANYTOWN_RAGECANDYBAR_PRICE EQU 300
 
 MahoganyTown_MapScripts:
 	def_scene_scripts
-	scene_script .DummyScene0 ; SCENE_DEFAULT
-	scene_script .DummyScene1 ; SCENE_FINISHED
+	scene_script .DummyScene0, SCENE_MAHOGANYTOWN_TRY_RAGECANDYBAR
+	scene_script .DummyScene1, SCENE_MAHOGANYTOWN_NOOP
 
 	def_callbacks
 	callback MAPCALLBACK_NEWMAP, .FlyPoint
@@ -254,8 +254,8 @@ MahoganyTown_MapEvents:
 	warp_event  9,  1, ROUTE_43_MAHOGANY_GATE, 3
 
 	def_coord_events
-	coord_event 19,  8, SCENE_DEFAULT, MahoganyTownTryARageCandyBarScript
-	coord_event 19,  9, SCENE_DEFAULT, MahoganyTownTryARageCandyBarScript
+	coord_event 19,  8, SCENE_MAHOGANYTOWN_TRY_RAGECANDYBAR, MahoganyTownTryARageCandyBarScript
+	coord_event 19,  9, SCENE_MAHOGANYTOWN_TRY_RAGECANDYBAR, MahoganyTownTryARageCandyBarScript
 
 	def_bg_events
 	bg_event  1,  5, BGEVENT_READ, MahoganyTownSign

maps/MrPokemonsHouse.asm
@@ -4,8 +4,8 @@
 
 MrPokemonsHouse_MapScripts:
 	def_scene_scripts
-	scene_script .MeetMrPokemon ; SCENE_DEFAULT
-	scene_script .DummyScene ; SCENE_FINISHED
+	scene_script .MeetMrPokemon, SCENE_MRPOKEMONSHOUSE_MEET_MR_POKEMON
+	scene_script .DummyScene,    SCENE_MRPOKEMONSHOUSE_NOOP
 
 	def_callbacks
 
@@ -120,7 +120,7 @@ MrPokemonsHouse_OakScript:
 	waitbutton
 	closetext
 	setevent EVENT_RIVAL_NEW_BARK_TOWN
-	setscene SCENE_FINISHED
+	setscene SCENE_MRPOKEMONSHOUSE_NOOP
 	setmapscene CHERRYGROVE_CITY, SCENE_CHERRYGROVECITY_MEET_RIVAL
 	setmapscene ELMS_LAB, SCENE_ELMSLAB_MEET_OFFICER
 	specialphonecall SPECIALCALL_ROBBED

maps/Route27.asm
@@ -11,8 +11,8 @@
 
 Route27_MapScripts:
 	def_scene_scripts
-	scene_script .DummyScene0 ; SCENE_DEFAULT
-	scene_script .DummyScene1 ; SCENE_FINISHED
+	scene_script .DummyScene0, SCENE_ROUTE27_FIRST_STEP_INTO_KANTO
+	scene_script .DummyScene1, SCENE_ROUTE27_NOOP
 
 	def_callbacks
 
@@ -40,7 +40,7 @@ FirstStepIntoKantoScene_Continue:
 	writetext Route27FisherText
 	waitbutton
 	closetext
-	setscene SCENE_FINISHED
+	setscene SCENE_ROUTE27_NOOP
 	end
 
 Route27FisherScript:
@@ -434,8 +434,8 @@ Route27_MapEvents:
 	warp_event 36,  5, TOHJO_FALLS, 2
 
 	def_coord_events
-	coord_event 18, 10, SCENE_DEFAULT, FirstStepIntoKantoLeftScene
-	coord_event 19, 10, SCENE_DEFAULT, FirstStepIntoKantoRightScene
+	coord_event 18, 10, SCENE_ROUTE27_FIRST_STEP_INTO_KANTO, FirstStepIntoKantoLeftScene
+	coord_event 19, 10, SCENE_ROUTE27_FIRST_STEP_INTO_KANTO, FirstStepIntoKantoRightScene
 
 	def_bg_events
 	bg_event 25,  7, BGEVENT_READ, TohjoFallsSign