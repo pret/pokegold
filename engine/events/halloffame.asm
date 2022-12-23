8818f05ab0fe9ddedcc8ad2453f80abb13b13090
Masaru2 <dmasaru3@gmail.com>
2022-12-23 20:06:33 -0300

    forgot one bugfix silly me

--- engine/events/halloffame.asm
+++ engine/events/halloffame.asm
@@ -16,6 +16,11 @@ HallOfFame::
 
 ; BUG: Entering the Hall of Fame without a save file can corrupt the PC boxes (see docs/bugs_and_glitches.md)
 
+	ld a, [wSavedAtLeastOnce]
+	and a
+	jr nz, .saved
+	farcall ErasePreviousSave
+.saved
 	ld hl, wHallOfFameCount
 	ld a, [hl]
 	cp HOF_MASTER_COUNT
