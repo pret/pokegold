pret/pokecrystal

more accurate script names: refreshscreen -> reanchormap, and reloadmappart -> refreshmap (#1078)

engine/overworld/scripting.asm
@@ -136,7 +136,7 @@ ScriptCommandTable:
 	dw Script_itemnotify                 ; 45
 	dw Script_pocketisfull               ; 46
 	dw Script_opentext                   ; 47
-	dw Script_refreshscreen              ; 48
+	dw Script_reanchormap                ; 48
 	dw Script_closetext                  ; 49
 	dw Script_writeunusedbyte            ; 4a
 	dw Script_farwritetext               ; 4b
@@ -188,7 +188,7 @@ ScriptCommandTable:
 	dw Script_changemapblocks            ; 79
 	dw Script_changeblock                ; 7a
 	dw Script_reloadmap                  ; 7b
-	dw Script_reloadmappart              ; 7c
+	dw Script_refreshmap                 ; 7c
 	dw Script_writecmdqueue              ; 7d
 	dw Script_delcmdqueue                ; 7e
 	dw Script_playmusic                  ; 7f
@@ -2157,7 +2157,7 @@ Script_changeblock:
 	call BufferScreen
 	ret
 
-Script_reloadmappart::
+Script_refreshmap::
 	xor a
 	ldh [hBGMapMode], a
 	call LoadOverworldTilemapAndAttrmapPals
@@ -2192,8 +2192,8 @@ Script_opentext:
 	call OpenText
 	ret
 
-Script_refreshscreen:
-	call RefreshScreen
+Script_reanchormap:
+	call ReanchorMap
 	call GetScriptByte
 	ret
 

pret/pokegold

engine/overworld/scripting.asm
@@ -136,7 +136,7 @@ ScriptCommandTable:
 	dw Script_itemnotify                 ; 45
 	dw Script_pocketisfull               ; 46
 	dw Script_opentext                   ; 47
-	dw Script_refreshscreen              ; 48
+	dw Script_reanchormap                ; 48
 	dw Script_closetext                  ; 49
 	dw Script_writeunusedbyte            ; 4a
 	dw Script_farwritetext               ; 4b
@@ -187,7 +187,7 @@ ScriptCommandTable:
 	dw Script_changemapblocks            ; 78
 	dw Script_changeblock                ; 79
 	dw Script_reloadmap                  ; 7a
-	dw Script_reloadmappart              ; 7b
+	dw Script_refreshmap                 ; 7b
 	dw Script_writecmdqueue              ; 7c
 	dw Script_delcmdqueue                ; 7d
 	dw Script_playmusic                  ; 7e
@@ -2041,7 +2041,7 @@ Script_changeblock:
 	call BufferScreen
 	ret
 
-Script_reloadmappart::
+Script_refreshmap::
 	xor a
 	ldh [hBGMapMode], a
 	call LoadOverworldTilemapAndAttrmapPals
@@ -2076,8 +2076,8 @@ Script_opentext:
 	call OpenText
 	ret
 
-Script_refreshscreen:
-	call RefreshScreen
+Script_reanchormap:
+	call ReanchorMap
 	call GetScriptByte
 	ret
 