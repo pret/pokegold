33df401392e9ab6ecebba0e4403bfd8387b77f22
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-08-11 16:36:33 -0400

    Identify `JOYP_SGB_MLT_REQ` constant

--- engine/gfx/color.asm
+++ engine/gfx/color.asm
@@ -908,8 +908,8 @@ PushSGBBorderPalsAndWait:
 	call _PushSGBPals
 	call SGBDelayCycles
 	ldh a, [rJOYP]
-	and $3
-	cp $3
+	and JOYP_SGB_MLT_REQ
+	cp JOYP_SGB_MLT_REQ
 	jr nz, .carry
 	ld a, JOYP_SGB_ZERO
 	ldh [rJOYP], a
@@ -936,8 +936,8 @@ endr
 	call SGBDelayCycles
 	call SGBDelayCycles
 	ldh a, [rJOYP]
-	and $3
-	cp $3
+	and JOYP_SGB_MLT_REQ
+	cp JOYP_SGB_MLT_REQ
 	jr nz, .carry
 	call .FinalPush
 	and a
